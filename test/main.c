11562a6dec38d45029c780f98746451c83b62fc6
mschafer <mschafer@alum.mit.edu>
2016-02-01 22:35:45 -0500

    vector and matrix tests

--- test/main.c
+++ test/main.c
@@ -1,15 +1,59 @@
 #include <stdio.h>
 #include <complex.h>
 #include <stdint.h>
+#include <String.h>
 #include "by_hand.h"
 
 char *foo = "abcxxxxxxxxxxxxxxxxxxxxx";
 char afoo[3][2] = { "a", "b", "c" };
 
+int run_vector_test()
+{
+    float v[3] = {1.f, 2.f, 3.f};
+    float s = 6.;
+    vector_test_(&s, v);
+    return (v[0] == 7. && v[1] == 8. && v[2] == 9.);
+}
+
+
+int run_vector_test2()
+{
+    float v[3] = {1.f, 2.f, 3.f};
+    float s = 6.;
+    int32_t n = 2;
+    vector_test2_(&s, v, &n);
+    return (v[0] == 7. && v[1] == 8. && v[2] == 3.);
+}
+
+int run_matrix_test()
+{
+    float m[3][4] = {
+        {1., 2., 3., 4.},
+        {1., 2., 3., 4.},
+        {1., 2., 3., 4.},        
+    };
+    
+    float a[3][4] = {
+        {1., 0., 3., 4.},
+        {1., 0., 3., 4.},
+        {1., 0., 3., 4.},        
+    };
+    
+    float s = -2.;
+    int32_t ncols = 3;
+    matrix_test_(&s, m, &ncols);
+    return !memcmp(m, a, sizeof(m));
+}
+
 int main(int argc, char *argv[])
 {
     double x = 2.;
     double y = times2_(&x);
+
+    printf("vector_test %d\n", run_vector_test());
+    printf("vector_test2 %d\n", run_vector_test2());
+    printf("matrix_test %d\n", run_matrix_test());
+    
 
     printf("TIMES2_(%g) = %g\n", x, y);
 
